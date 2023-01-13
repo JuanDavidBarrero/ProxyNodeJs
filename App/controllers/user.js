6443512475ffa17b8369c5b11f7363f55656cd19
Juan David Barrero L <Juandbarrero@hotmail.com>
2023-01-12 20:49:13 -0500

    Post put delete methods added

--- App/controllers/user.js
+++ App/controllers/user.js
@@ -6,3 +6,40 @@ export const usuariosGet = async (req = request, res = response) => {
         edad: 18
     })
 }
+
+export const postUsuario = async (req = request, res = response) => {
+
+    const { nombre, desc } = req.body;
+
+    return res.json({
+        status:true,
+        nombre,
+        desc,
+        msg:"completado"
+    })
+}
+
+export const PutUsuario = async (req = request, res = response) => {
+
+    const id  = req.params.id;
+
+    const { nombre } = req.body;
+
+
+    return res.json({
+        status:true,    
+        nombre,
+        id,
+        msg:"Actualizado"
+    })
+}
+
+export const DeleteUsuario = async (req = request, res = response) => {
+
+    const id  = req.params.id;
+
+    return res.json({
+        status:true,    
+        msg:`Usuario con el id ${id} borrado`
+    })
+}
--- App/routes/user.js
+++ App/routes/user.js
@@ -1,8 +1,11 @@
 import {Router} from "express";
-import { usuariosGet } from "../controllers/user.js";
+import { DeleteUsuario, postUsuario, PutUsuario, usuariosGet } from "../controllers/user.js";
 
 const router =  Router();
 
 router.get('/', usuariosGet);
+router.post('/', postUsuario);
+router.put('/:id', PutUsuario);
+router.delete('/:id', DeleteUsuario);
 
 export default router;
